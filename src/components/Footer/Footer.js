232846b5dda856da1abaa7393f3665e835e3a179
Samantha Martinelli <brynhilde@hotmail.fr>
2024-01-03 17:46:48 +0100

    Changement année dynamique dans le footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -3,9 +3,13 @@ import { Link } from "react-router-dom";
 
 
 export default function Footer() {
+
+  const date = new Date();
+  const year = date.getFullYear();
+
   return (
     <footer>
-      <div className="footer-copyright">© 2023 - Tous droits réservés - Samantha Martinelli</div>
+      <div className="footer-copyright">© {year} - Tous droits réservés - Samantha Martinelli</div>
       <Link to="/privacy" className="footer-link-one">Politique de confidentialité</Link>
       <Link to="/notice" className="footer-link-two">Mentions légales</Link>
     </footer>
--- src/components/Icons.js
+++
@@ -1,7 +0,0 @@
-import IconsList from "icons.js";
-
-export default function Icons({name}) {
-  const IconComponent = IconsList[name];
-  
-  return <IconComponent/>;
-}
