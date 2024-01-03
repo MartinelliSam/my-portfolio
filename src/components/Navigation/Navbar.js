984b8a41cab75dc7177dc7770b9dd7ba4fb616c2
Samantha Martinelli <brynhilde@hotmail.fr>
2024-01-03 17:49:37 +0100

    Changement année dynamique dans le footer

---
+++ src/components/Navigation/Navbar.js
@@ -0,0 +1,36 @@
+import './Navbar.css';
+import { useEffect } from 'react';
+import { HashLink as Link } from 'react-router-hash-link';
+import Logo from 'assets/BlackWhiteMinimalistLogo@syammasfitria.png';
+
+export default function Navbar({ burgerOpen }) {
+
+
+  useEffect(() => {
+    const glass = document.getElementById('glass-bar');
+    window.addEventListener('scroll', () => {
+      if(window.innerWidth > 990) {
+        if (window.scrollY < 800) {
+          glass.className = '';
+        } else {
+          glass.className = 'glass';
+        }
+      } 
+    });
+  }, []);
+
+  return (
+    <nav>
+      <ul id='glass-bar' className={`${burgerOpen ? "navbar-show" : "navbar-hide"}`}>
+        <img src={Logo} alt='logo'/>
+        <li><Link to="#about" className='navlink'>A propos</Link></li>
+        <li><Link to="#projects" className='navlink'>Mes projets</Link></li>
+        <li><Link to="#skills" className='navlink'>Compétences</Link></li>
+        <li><Link to="#contact" className='navlink'>Contact</Link></li>
+      </ul>
+  </nav>
+  )
+}
+
+
+
---
+++ src/components/Skills/Skills.js
@@ -0,0 +1,91 @@
+import "./Skills.css";
+import Icons from "components/Icons.js";
+import HeartIco from 'assets/heart-svgrepo-com.svg';
+
+export default function Skills() {
+  return (
+    <div id="skills">
+      <h2>Mes compétences</h2>
+      <div id="skills-container">
+        <div>
+          <h3>Front</h3>
+          <div id="front-skills">
+            <div className="skill-icon">
+              <Icons name="HTML" />
+              HTML
+            </div>
+            <div className="skill-icon">
+              <Icons name="CSS" />
+              CSS3
+            </div>
+            <div className="skill-icon js-ico">
+            <img src={HeartIco} alt="heart-ico" className="heart-ico"/>
+              <Icons name="JS" />
+              JavaScript
+            </div>
+            <div className="skill-icon react-ico">
+              <img src={HeartIco} alt="heart-ico" className="heart-ico"/>
+              <Icons name="React" />
+              React.js
+            </div>
+            <div className="skill-icon">
+              <Icons name="Bootstrap" />
+              Bootstrap
+            </div>
+          </div>
+        </div>
+        <div>
+          <h3>Back</h3>
+          <div id="back-skills">
+            <div className="skill-icon">
+              <Icons name="PHP" />
+              PHP
+            </div>
+            <div className="skill-icon">
+              <Icons name="Symfony" />
+              Symfony
+            </div>
+          </div>
+        </div>
+        <div>
+          <h3>Bases de données</h3>
+          <div id="database-skills">
+            <div className="skill-icon">
+              <Icons name="MariaDB" />
+              MariaDB
+            </div>
+            <div className="skill-icon">
+              <Icons name="MySQL" />
+              MySQL
+            </div>
+          </div>
+        </div>
+        <div>
+          <h3>Outils</h3>
+          <div id="tools-skills">
+            <div className="skill-icon">
+              <Icons name="Git" />
+              Git
+            </div>
+            <div className="skill-icon">
+              <Icons name="NPM" />
+              NPM
+            </div>
+            <div className="skill-icon">
+              <Icons name="Composer" />
+              Composer
+            </div>
+            <div className="skill-icon">
+              <Icons name="Jira" />
+              Jira
+            </div>
+            <div className="skill-icon">
+              <Icons name="Figma" />
+              Figma
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
