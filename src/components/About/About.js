409e6415a2c741a7c69a3a877424a5268ce13001
Samantha Martinelli <brynhilde@hotmail.fr>
2024-03-19 19:24:31 +0100

    Modification du texte A propos

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -5,21 +5,27 @@ export default function About() {
     <div id="about">
       <h2>À propos de moi</h2>
       <div>
-        <p>Bonjour ! <span className="wave">👋</span></p>
         <p>
-          Je m'appelle Samantha Martinelli, et je suis développeuse fullstack junior.
-          J'ai travaillé plusieurs années dans le domaine de l'aérien et de la logistique (catering, assistance, fret),
-          ce qui m'a permis d'acquérir plusieurs compétences, telles que le travail d'équipe, le sens de 
-          l'écoute, la rigueur et la gestion du stress.
+          Bonjour ! <span className="wave">👋</span>
         </p>
         <p>
-          La découverte du code et l'univers du développement ont fait naître une telle passion, que j'ai 
-          entrepris de me reconvertir en tant que développeuse fullstack, en parallèle de mon emploi.  
+          Je m'appelle Samantha Martinelli, et je suis développeuse fullstack
+          junior. J'ai travaillé plusieurs années dans le domaine de l'aérien et
+          de la logistique (catering, assistance, fret), ce qui m'a permis
+          d'acquérir plusieurs compétences, telles que le travail d'équipe, le
+          sens de l'écoute, la rigueur et la gestion du stress.
         </p>
         <p>
-          Ayant fraîchement obtenu le Titre Professionnel de Développeur Web/Web Mobile (Bac +2), 
-          je suis prête à intégrer une entreprise qui saura me permettre de renforcer mes nouvelles compétences, 
-          et j'ai hâte de pouvoir exercer ce métier qui me fascine tant.
+          La découverte du code et l'univers du développement ont fait naître
+          une telle passion, que j'ai entrepris de me reconvertir en tant que
+          développeuse fullstack, en parallèle de mon emploi.
+        </p>
+        <p>
+          Diplômée d'un Titre Professionnel de Développeur Web/Web Mobile (Bac
+          +2), je prépare cette année le titre RNCP de niveau bac +3/+4 de
+          développeur d'applications, et je suis à la recherche d'un contrat de
+          professionnalisation en alternance en tant que développeuse
+          full-stack.
         </p>
       </div>
     </div>
