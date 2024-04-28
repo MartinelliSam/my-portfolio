faf40f3ff7e055593f7b6ce9238a4ee7e1d08881
Samantha Martinelli <brynhilde@hotmail.fr>
2024-04-28 18:16:03 +0200

    Modification texte de présentation

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -12,7 +12,7 @@ export default function About() {
           Je m'appelle Samantha, et je suis développeuse fullstack junior. J'ai
           travaillé plusieurs années dans le domaine de l'aérien et de la
           logistique (catering, assistance, fret), ce qui m'a permis d'acquérir
-          plusieurs compétences, telles que le travail d'équipe, le sens de
+          plusieurs compétences, telles que l'esprit d'équipe, le sens de
           l'écoute, la rigueur et la gestion du stress.
         </p>
         <p>
@@ -21,11 +21,17 @@ export default function About() {
           développeuse, en parallèle de mon emploi.
         </p>
         <p>
-          Diplômée en tant que Développeuse Web/Web Mobile (Bac +2), je prépare
-          cette année le titre RNCP de niveau bac +3/+4 de Développeuse
-          d'applications, et je suis à la recherche d'un contrat de
-          professionnalisation en alternance en tant que développeuse
-          full-stack.
+          Diplômée en tant que Développeuse Web/Web Mobile (Bac +2), j'occupe
+          actuellement le poste de Test Manager, où je suis responsable de la
+          planification et de l'exécution des tests logiciels pour garantir la
+          qualité des produits développés.
+        </p>
+        <p>
+          À travers ce portfolio, je souhaite partager avec vous mes
+          réalisations et mes projets, fruits de mon apprentissage continu. Je
+          suis déterminée à continuer à apprendre à coder, afin de consolider
+          mes acquis et d'acquérir des connaissances qui me seront précieuses en
+          tant que Test Manager.
         </p>
       </div>
     </div>
