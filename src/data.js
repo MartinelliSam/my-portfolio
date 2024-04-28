d809fd0fe1ca2360a56496a569e168877a660ba1
Samantha Martinelli <brynhilde@hotmail.fr>
2024-04-28 18:24:01 +0200

    Modification de la description de la Card Portfolio dans les projets

--- src/data.js
+++ src/data.js
@@ -11,57 +11,57 @@ const ProjectsData = [
     image: QuaiAntique,
     name: "Quai Antique",
     stack: [
-      <Icons name="Symfony" key="SymfonyIco"/>,
-      <Icons name="Bootstrap" key="BootstrapIco"/>,
-      <Icons name="JS" key="JsIco"/>,
-      <Icons name="MariaDB" key="MariaDbIco"/>
+      <Icons name="Symfony" key="SymfonyIco" />,
+      <Icons name="Bootstrap" key="BootstrapIco" />,
+      <Icons name="JS" key="JsIco" />,
+      <Icons name="MariaDB" key="MariaDbIco" />,
     ],
     github: "https://github.com/MartinelliSam/Projet-QuaiAntique",
     source: "https://restaurant-lequaiantique.herokuapp.com/",
     description:
-      "Site du restaurant le Quai Antique, développé sous Symfony. Module de réservation et panneau d'administration"
+      "Site du restaurant le Quai Antique, développé sous Symfony. Module de réservation et panneau d'administration",
   },
   {
     id: "portfolio",
     image: Portfolio,
     name: "Portfolio",
     stack: [
-      <Icons name="React" key="ReactIco"/>,
-      <Icons name="CSS" key="CssIco"/>
+      <Icons name="React" key="ReactIco" />,
+      <Icons name="CSS" key="CssIco" />,
     ],
     github: "https://github.com/MartinelliSam/my-portfolio",
     source: "https://www.samantha-martinelli.dev/",
     description:
-      "Portfolio de présentation de mes projets (Single Page Application). Page où vous vous trouvez actuellement"
+      "Portfolio de présentation de mes projets. On s'y trouve actuellement 🤓. Je compte le faire évoluer, notamment en planifiant une refonte en incluant TypeScript",
   },
   {
     id: "spy-agency",
     image: SpyAgency,
     name: "Spy Agency",
     stack: [
-      <Icons name="Bootstrap" key="BootstrapIco"/>,
-      <Icons name="PHP" key="PhpIco"/>,
-      <Icons name="MariaDB" key="MariaDbIco"/>,
-      <Icons name="JS" key="JsIco"/>
+      <Icons name="Bootstrap" key="BootstrapIco" />,
+      <Icons name="PHP" key="PhpIco" />,
+      <Icons name="MariaDB" key="MariaDbIco" />,
+      <Icons name="JS" key="JsIco" />,
     ],
     github: "https://github.com/MartinelliSam/Spy_agency",
     source: "https://services-secrets-d79da3f75178.herokuapp.com/",
     description:
-      "Site de gestion d'une agence d'espion avec interface back-office, à l'aide de PHP."
+      "Site de gestion d'une agence d'espion avec interface back-office, à l'aide de PHP.",
   },
   {
     id: "dice-game",
     image: DiceGame,
     name: "Dice Game",
     stack: [
-      <Icons name="HTML" key="HtmlIco"/>,
-      <Icons name="CSS" key="CssIco"/>,
-      <Icons name="JS" key="JsIco"/>
+      <Icons name="HTML" key="HtmlIco" />,
+      <Icons name="CSS" key="CssIco" />,
+      <Icons name="JS" key="JsIco" />,
     ],
     github: "https://github.com/MartinelliSam/JeuDeDes",
     source: "https://great-dice-game.netlify.app/",
     description:
-      "Création d'un jeu de dés dynamique sur navigateur à l'aide de Javascript"
+      "Création d'un jeu de dés dynamique sur navigateur à l'aide de Javascript",
   },
 ];
 
